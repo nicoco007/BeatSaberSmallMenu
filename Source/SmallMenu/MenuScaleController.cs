80504d1b49b84bbfccf32a76f042a122804fe6f2
Nicolas Gnyra <nicolas@gnyra.com>
2021-02-27 18:48:16 -0500

    Use ScreenModeController to apply position/scale

--- Source/SmallMenu/MenuScaleController.cs
+++ Source/SmallMenu/MenuScaleController.cs
@@ -1,4 +1,5 @@
 ﻿using HMUI;
+using IPA.Utilities;
 using System;
 using System.ComponentModel;
 using UnityEngine;
@@ -9,18 +10,22 @@ namespace SmallMenu
 {
     internal class MenuScaleController : IInitializable, IDisposable
     {
+        private static readonly FieldAccessor<ScreenModeController, ScreenModeData>.Accessor kDefaultModeDataAccessor = FieldAccessor<ScreenModeController, ScreenModeData>.GetAccessor("_defaultModeData");
+
         private Logger _logger;
         private Settings _settings;
         private HierarchyManager _hierarchyManager;
+        private ScreenModeController _screenModeController;
 
         private Transform _bottomScreen;
 
         [Inject]
-        public void Construct(Logger logger, Settings settings, HierarchyManager hierarchyManager)
+        public void Construct(Logger logger, Settings settings, HierarchyManager hierarchyManager, ScreenModeController screenModeController)
         {
             _logger = logger;
             _settings = settings;
             _hierarchyManager = hierarchyManager;
+            _screenModeController = screenModeController;
         }
 
         public void Initialize()
@@ -53,18 +58,24 @@ namespace SmallMenu
 
         private void UpdateScale()
         {
-            if (!_bottomScreen || _settings.scale <= 0) return;
+            if (_settings.scale <= 0) return;
+
+            ScreenModeData screenModeData = kDefaultModeDataAccessor(ref _screenModeController);
 
             // menu screen (scale 1.0) was originally at y = 1.35 and at scale 1.5 it is at y = 0.85
             // therefore, at scale 1.0, the screen system must be moved up by y += 0.5
             float offset = Mathf.Max(0, 1.5f - 1f * _settings.scale);
 
-            Vector3 screenSystemPosition = _hierarchyManager.transform.localPosition;
-            _hierarchyManager.transform.localPosition = new Vector3(screenSystemPosition.x, offset, screenSystemPosition.z);
-            _hierarchyManager.transform.localScale = Vector3.one * _settings.scale;
+            screenModeData.position.y = offset;
+            screenModeData.scale = _settings.scale;
 
-            Vector3 bottomScreenPosition = _bottomScreen.localPosition;
-            _bottomScreen.localPosition = new Vector3(bottomScreenPosition.x, (0.02f - offset) / _settings.scale, bottomScreenPosition.z);
+            _screenModeController.SetDefaultMode();
+
+            if (!_bottomScreen) return;
+
+            Vector3 bottomScreenPosition = _bottomScreen.position;
+            bottomScreenPosition.y = 0.02f;
+            _bottomScreen.position = bottomScreenPosition;
         }
     }
 }
